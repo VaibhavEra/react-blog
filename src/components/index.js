4c588f04d7925708baa30b30effca45fb6ded464
Vaibhav Saini <145651754+VaibhavEra@users.noreply.github.com>
2025-07-04 12:36:00 +0000

    add components

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,7 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
 
-export { Header, Footer, Logo, Container, LogoutBtn };
+export { Header, Footer, Logo, Container, LogoutBtn, Button, Input };
